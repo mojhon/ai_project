ade8a07236a1e97372868fc196f40c3c1fafe4d1
mojhon <158581564+mojhon@users.noreply.github.com>
2024-02-08 23:48:25 +0100

    Update hasbulla.py

--- hasbulla.py
+++ hasbulla.py
@@ -17,13 +17,13 @@ def step(robotId, sensors):
 	#les robots 2 et 7 changent de comportement apres 50 puis 150 puis 50 et ainsi de suite
 	#les robots 3 à 6 executent uniquement le randomizer
 	if robotId%8 == 1 or robotId%8 == 6 and (count % 100) < 50: #chaque 50 iterations
-		return explorer.step(robotId,sensors)
+		return suivre.step(robotId,sensors)
 	if robotId%8 >= 2 and robotId%8 <= 5 :
-		return randomizer.step(robotId,sensors)
+		return explorer.step(robotId,sensors)
 	if(robotId%8 == 0):
 		count += 1	
 	if count % 200 < 100:#chaque 100 
-		return randomizer.step(robotId,sensors)
-	else:
 		return explorer.step(robotId,sensors)
+	else:
+		return suivre.step(robotId,sensors)
 
