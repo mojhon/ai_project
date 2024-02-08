085ad0f1f87c15adbbf40411e0d3d5b05df4ae32
mojhon <158581564+mojhon@users.noreply.github.com>
2024-02-08 23:54:01 +0100

    Update hasbulla.py

--- hasbulla.py
+++ hasbulla.py
@@ -15,7 +15,7 @@ def step(robotId, sensors):
 	global count
 	#les robots 1 et 8 changent de comportement chaque 100 iterations
 	#les robots 2 et 7 changent de comportement apres 50 puis 150 puis 50 et ainsi de suite
-	#les robots 3 à 6 executent uniquement le randomizer
+	#les robots 3 à 6 executent uniquement le explorer
 	if robotId%8 == 1 or robotId%8 == 6 and (count % 100) < 50: #chaque 50 iterations
 		return suivre.step(robotId,sensors)
 	if robotId%8 >= 2 and robotId%8 <= 5 :
