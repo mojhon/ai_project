d0bfed5e497981bce3687c6be077ee45a8e88855
mojhon <158581564+mojhon@users.noreply.github.com>
2024-02-09 01:06:37 +0100

    Update suivre.py

--- suivre.py
+++ suivre.py
@@ -71,7 +71,7 @@ def step(robotId, sensors): # <<<<<<<<<------- fonction à modifier pour le TP1
     if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == True:  # if allié devant
         while rotation == 0:  #pour etre sur que le robot ne suit pas son camarade
             rotation = random.uniform(-1, 1) 
-    	return translation, rotation
+	return translation, rotation
 
     # if allié devant à gauche
     if sensors["sensor_front_left"]["isRobot"] == True and sensors["sensor_front_left"]["isSameTeam"] == True:
